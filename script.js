27e64e9be2e8f63306026ef7aa000efbf2dd2425
omerfaruk2626 <eseskikieses@gmail.com>
2024-02-19 13:41:34 +0300

    update

--- script.js
+++ script.js
@@ -1,31 +1,44 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuButton = document.querySelector('.menu-button');
-    const navList = document.querySelector('.ul');
 
-    menuButton.addEventListener('click', function () {
-        navList.classList.toggle('active');
-    });
+//! -----------------------RADİO--------------------//
+
+var radio = document.getElementById("radio");
+
+function toggleRadio() {
+  if (radio.paused) {
+    radio.play();
+  } else {
+    radio.pause();
+  }
+  radio.volume = 0.03;
+}
+
+function pauseRadio() {
+  radio.pause();
+}
+
+function setVolume(volume) {
+  radio.volume = parseFloat(volume);
+}
+
+radio.addEventListener('playing', function () {
+  radio.volume = 0.03;
 });
 
+//! -----------------------SECTİON--------------------//
+
+function showSection(sectionId) {
+  // Tüm bölümleri gizle
+  var sections = document.querySelectorAll('#main-content section');
+  sections.forEach(function (section) {
+    section.style.display = 'none';
+  });
+
+  // Belirli bölümü göster
+  var selectedSection = document.getElementById(sectionId);
+  if (selectedSection) {
+    selectedSection.style.display = 'block';
+  }
+}
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    const dateElement = document.querySelector('.date');
-    const menuButton = document.querySelector('.menu-button');
-    const navList = document.querySelector('.ul');
 
-    // Tarih bilgisini al
-    const currentDate = new Date();
-    const day = currentDate.getDate();
-    const month = currentDate.toLocaleString('default', { month: 'long' }); // Türkçe ay ismi
-    const year = currentDate.getFullYear();
-    const dayName = currentDate.toLocaleString('default', { weekday: 'long' }); // Türkçe gün ismi
-
-    const formattedDate = `${day} ${month} ${year}, ${dayName}`;
-    dateElement.textContent = formattedDate;
-
-    // Menü açma/kapatma işlemleri
-    menuButton.addEventListener('click', function () {
-        navList.classList.toggle('active');
-    });
-});
