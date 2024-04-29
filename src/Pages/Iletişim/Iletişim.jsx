a0d022422ec870fd6fa92ee17913d9c1a89fd552
omerfaruk2626 <eseskikieses@gmail.com>
2024-04-29 17:40:55 +0300

    build

--- src/Pages/Iletişim/Iletişim.jsx
+++ src/Pages/Iletişim/Iletişim.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 const İletişim = () => {
   return (
-    <div>İletişim</div>
-  )
+    <div>
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+        <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="formFile" className="form-label">Default file input example</label>
+        <input className="form-control" type="file" id="formFile" />
+      </div>
+    </div>
+  );
 }
 
-export default İletişim
+export default İletişim;
